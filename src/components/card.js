bd7a908bd343d1dbbd563355b060a9a11397bab9
fabnupi23 <31360808+fabnupi23@users.noreply.github.com>
2022-08-17 12:23:59 -0500

    Instalación Bootstrap e importar estilos

---
+++ src/components/card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function card() {
+  return (
+    <div className='card'>
+        <img src="" alt=""/>
+        <div className='card-body'>
+            <h4 className='card-title'>Mi Titulo</h4>
+            <p className='card-text text-secondary'>Bootstrap es una biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web. Contiene plantillas de diseño con tipografía, formularios, botones, cuadros, menús de navegación y otros elementos de diseño basado en HTML y CSS, así como extensiones de JavaScript adicionales. A diferencia de muchos frameworks web, solo se ocupa del desarrollo front-end.</p>
+        </div>
+    </div>
+  )
+}
+
+export default card;
